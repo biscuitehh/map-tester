b97a56b284601d3b965e6f4ebf2c78d897eed898
Michael Thomas <michael.thomas@biscuitlabs.com>
2015-05-17 07:24:46 -0400

    Initial Commit

---
+++ Map Tester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Map Tester
+//
+//  Created by Michael Thomas on 5/17/15.
+//  Copyright (c) 2015 Biscuit Labs, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
